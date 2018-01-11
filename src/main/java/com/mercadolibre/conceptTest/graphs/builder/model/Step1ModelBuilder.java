b524b621e0197eb6f3c22c616c18db7d96437da4
Mariano Sosto <mariano.sosto@mercadolibre.com>
2018-01-11 16:13:12 -0300

    add pksModelbuilder and finderModelBuilder

--- src/main/java/com/mercadolibre/conceptTest/graphs/builder/model/Step1ModelBuilder.java
+++ src/main/java/com/mercadolibre/conceptTest/graphs/builder/model/Step1ModelBuilder.java
@@ -1,5 +1,7 @@
 package com.mercadolibre.conceptTest.graphs.builder.model;
 
+import com.mercadolibre.conceptTest.graphs.builder.model.attribute.pks.PksModelBuilder;
+import com.mercadolibre.conceptTest.graphs.builder.model.finder.FinderModelBuilder;
 import com.mercadolibre.conceptTest.graphs.model.Step1Model;
 import com.mercadolibre.flux.flow.graph.navigation.Context;
 
@@ -8,8 +10,19 @@ import com.mercadolibre.flux.flow.graph.navigation.Context;
  */
 public class Step1ModelBuilder {
 
+    private PksModelBuilder pksModelBuilder;
+    private FinderModelBuilder finderModeBuilder;
+
+    public Step1ModelBuilder() {
+        pksModelBuilder = new PksModelBuilder();
+        finderModeBuilder = new FinderModelBuilder();
+    }
+
     public Step1Model getModel(Context context) {
-        return new Step1Model(); // todo
+        Step1Model model = new Step1Model();
+        model.setPksModel(pksModelBuilder.build(context));
+        model.setFinderModel(finderModeBuilder.build(context));
+        return model;
     }
 
 }
