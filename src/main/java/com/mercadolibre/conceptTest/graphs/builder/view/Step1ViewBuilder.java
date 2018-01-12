aa9a848a0708cfe39bde1283f0fdc579454e5c21
mlizarraga1986 <matias.lizarraga@mercadolibre.com>
2018-01-12 17:04:23 -0300

    Add hidden and pks

--- src/main/java/com/mercadolibre/conceptTest/graphs/builder/view/Step1ViewBuilder.java
+++ src/main/java/com/mercadolibre/conceptTest/graphs/builder/view/Step1ViewBuilder.java
@@ -1,5 +1,7 @@
 package com.mercadolibre.conceptTest.graphs.builder.view;
 
+import com.mercadolibre.conceptTest.graphs.builder.view.contracts.InputHiddenViewContract;
+import com.mercadolibre.conceptTest.graphs.builder.view.contracts.attribute.pks.PksInput;
 import com.mercadolibre.conceptTest.graphs.builder.view.contracts.category.CategoryBreadcrumbViewContract;
 import com.mercadolibre.conceptTest.graphs.builder.view.contracts.category.CategorySelectionViewContract;
 import com.mercadolibre.conceptTest.graphs.builder.view.contracts.finder.FinderViewContract;
@@ -7,24 +9,26 @@ import com.mercadolibre.conceptTest.graphs.model.*;
 import com.mercadolibre.kisc.viewbuilder.Component;
 import com.mercadolibre.kisc.viewbuilder.template.Template;
 
+import java.util.List;
+
 /**
  * Created by msosto on 1/10/18.
  */
 public class Step1ViewBuilder {
 
-    public Component build(Step1Model model) {
-
+    public List<Component> build(Step1Model step1Model) {
         final Template<Step1Model, Step1Model> step1Template = Template.create(Step1Model.class).id("STEP_1").uiType("STEP");
         step1Template.addChild(getFinderTask());
         step1Template.addChild(getCategorySelectionTask());
+        step1Template.addChild(getPKsTask());
+        return step1Template.toComponents(step1Model, null).get();
     }
 
-    protected <TASK_MODEL extends TaskModel, STEP_MODEL> Template<TASK_MODEL, STEP_MODEL> getTask(Class<TASK_MODEL> modelClass, Template<TASK_MODEL, TASK_MODEL> body) {
-        final Template<TASK_MODEL, STEP_MODEL> task = Template.create(modelClass).id("CATEGORY_SELECTION" + "TASK").uiType("TASK").dataBuilder(finderModel ->);
+    protected <TASK_MODEL extends TaskModel, STEP_MODEL> Template<TASK_MODEL, STEP_MODEL> getTask(Class<STEP_MODEL> stepModelClass, Class<TASK_MODEL> taskModelClass, Template<TASK_MODEL, TASK_MODEL> body) {
+        final Template<TASK_MODEL, STEP_MODEL> task = Template.create(null, taskModelClass, stepModelClass).id("CATEGORY_SELECTION" + "TASK").uiType("TASK");
         task.addChild(HeaderModel.class, TaskModel::getHeaderModel).id("HEADER").uiType("HEADER");
         task.addChild(body);
         task.addChild(FooterModel.class, TaskModel::getFooterModel).id("FOOTER").uiType("FOOTER");
-
         return task;
     }
 
@@ -36,7 +40,7 @@ public class Step1ViewBuilder {
                         .withDisabled(finderModel.getDisabled())
                         .withValidationErrors(finderModel.getValidationErrors())
         );
-        return getTask(FinderModel.class, body);
+        return getTask(Step1Model.class, FinderModel.class, body);
     }
 
     protected Template<CategorySelectionModel, Step1Model> getCategorySelectionTask() {
@@ -45,7 +49,14 @@ public class Step1ViewBuilder {
                 new CategoryBreadcrumbViewContract().withCategories(categorySelectionModel.getBreadcrumbCategories())
         );
         body.addChild().id("CATEGORY_SELECTION").uiType("CATEGORY_SELECTION").dataBuilder(categorySelectionModel -> new CategorySelectionViewContract().withColumn(categorySelectionModel.getColumn()).withAdultContent(categorySelectionModel.getAdultContent()));
-        //TODO add input hidden
-        return getTask(CategorySelectionModel.class, body);
+        body.addChild().id("HIDDEN_CATEGORY_ID").uiType("HIDDEN").dataBuilder(categorySelectionModel -> new InputHiddenViewContract().withOutput(CategorySelectionModel.ITEM_CATEGORY_ID_OUTPUT).withValue(categorySelectionModel.getCategoryId()));
+        body.addChild().id("HIDDEN_CATALOG_PRODUCT_ID").uiType("HIDDEN").dataBuilder(categorySelectionModel -> new InputHiddenViewContract().withOutput(CategorySelectionModel.ITEM_CATALOG_PRODUCT_ID_OUTPUT).withValue(categorySelectionModel.getCatalogProductId()));
+        return getTask(Step1Model.class, CategorySelectionModel.class, body);
+    }
+
+    protected Template<PksModel, Step1Model> getPKsTask() {
+        final Template<PksModel, PksModel> body = Template.create(PksModel.class).id("BODY").uiType("BODY");
+        body.addChild().id("PKS").uiType("PKS").dataBuilder(pksModel -> new PksInput().withPksAttributes(pksModel.getPksAttributes()).withDecimalSeparator(pksModel.getDecimalSeparator()));
+        return getTask(Step1Model.class, PksModel.class, body);
     }
 }
