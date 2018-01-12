394d502f36c92c0f0bf7ea59e374e52a58fbc509
mlizarraga1986 <matias.lizarraga@mercadolibre.com>
2018-01-12 12:17:35 -0300

    Little fix

--- src/main/java/com/mercadolibre/conceptTest/graphs/Step1Builder.java
+++ src/main/java/com/mercadolibre/conceptTest/graphs/Step1Builder.java
@@ -1,11 +1,13 @@
 package com.mercadolibre.conceptTest.graphs;
 
 import com.mercadolibre.conceptTest.graphs.builder.model.Step1ModelBuilder;
+import com.mercadolibre.conceptTest.graphs.builder.view.Step1ViewBuilder;
 import com.mercadolibre.conceptTest.graphs.model.Step1Model;
 import com.mercadolibre.flux.flow.action.Action;
 import com.mercadolibre.flux.flow.exception.GraphNavigationException;
 import com.mercadolibre.flux.flow.graph.navigation.Context;
 import com.mercadolibre.flux.flow.graph.relationship.Achieve;
+import com.mercadolibre.kisc.viewbuilder.Component;
 
 /**
  * Created by msosto on 1/10/18.
@@ -18,7 +20,8 @@ public class Step1Builder extends Action {
 
     @Override
     protected void runAction() throws GraphNavigationException {
-        Step1Model model = new Step1ModelBuilder().getModel(context);
-//        Step1View step1View = new Step1ViewBuilder().build(model);
+        Step1Model step1Model = new Step1ModelBuilder().getModel(context);
+        Component step1View = new Step1ViewBuilder().build(step1Model);
+        //TODO Que hacemos con este component?
     }
 }
